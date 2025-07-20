54ec70e8c7ec27d2b89f3c36d02921e0419a0076
Ankit singh <as1230641@gmail.com>
2025-07-20 13:00:20 +0530

    added placeholder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useRef, useState } from 'react';
 
 function App() {
+  const [text,setText] = useState("")
+  const [jumpLine, setJumpLine] = useState("");
+  const editorRef = useRef(null);
+  const lineRef = useRef(null);
+  const [highlight, sethightlight] = useState(null)
+  
+  const handleScroll= () =>{
+    lineRef.current.scrollTop = editorRef.current.scrollTop;
+  };
+
+  const handleJump = () =>{
+    const lines = text.split("\n");
+    const lineHeight = 20;
+    const lineNum = parseInt(jumpLine,10)
+
+    if(!isNaN(lineNum) && lineNum >= 1 && lineNum <= lines.length){
+      const scrollValue = (lineNum -1) * lineHeight;
+      editorRef.current.scrollTop = scrollValue;
+      lineRef.current.scrollTop = scrollValue;
+      sethightlight(lineNum)
+
+    }else{
+      alert("invalid line number.")
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div>
+      <input
+        type='number'
+        placeholder='Enter line Number'
+        value={jumpLine}
+        onChange={(e)=> setJumpLine(e.target.value)}
+      />
+      <button onClick={handleJump}>Jump</button>
     </div>
+    <div style={{ display:"flex", height:"300px",border:"1px solid black"}}>
+      <div ref={lineRef}
+        style={
+          {width:"20px",
+            background: '#f0f0f0',
+            overflow:"auto",
+            textalign:"right",
+            paddingRight:6,
+            fontSize:14
+          }
+        }
+      >
+
+        {text.split("\n").map((_,i) =>(
+          <div key={i} style={{
+            height:20,
+            background: highlight === i+1?"orange": "transparent"
+          }}> 
+            {i + 1}
+          </div>
+        ))}
+      </div>
+
+      <textarea 
+        ref={editorRef}
+        value={text}
+        onChange={(e)=> setText(e.target.value)}
+        onScroll={(handleScroll)}
+        style={{
+          flex:1,
+          height:"100%",
+          padding:"5px 10px",
+          fontFamily:'monospace',
+          fontSize:14,
+          lineHeight:"scroll",
+          border:"none",
+          outline:"none"
+
+        }}
+        />
+    </div>
+  </div>
   );
 }
 
